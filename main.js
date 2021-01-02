4269c508e40897803aeb3cf70c80efb0bc4f5dd2
Bharati <bharatisharada@gmail.com>
2021-01-02 18:59:38 +0530

    Log to console

---
+++ main.js
@@ -0,0 +1,29 @@
+// Single line comments
+/*
+    Multi
+    line
+    Comments
+*/
+
+// Log to console
+console.log('Hello world');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+console.error(greeting); //Shows the greeting variable content as an error in Red color
+console.clear(); //clears the console
+console.warn("This is a warning"); //similar to error but prints in yellow
+console.time('Hello'); //Hello is an identifier
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+console.timeEnd('Hello'); //Time taken from beginning of time 'Hello' to the end.
+
+
