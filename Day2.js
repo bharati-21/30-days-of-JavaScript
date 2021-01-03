cc35c9eaad223c9a895d366305c157d20d5c5711
Bharati <bharatisharada@gmail.com>
2021-01-03 20:02:34 +0530

    Edited JS and HTML files

--- Day2.js
+++ Day2.js
@@ -8,7 +8,8 @@
     Comments
 */
 
-/*
+/*********************************************************************************/
+
 //2. CONSOLE.LOG()
     // Log to console
     console.log('Hello world');
@@ -30,14 +31,17 @@
         console.log('Hello world');
         console.log('Hello world');
     console.timeEnd('Hello'); //Time taken from beginning of time 'Hello' to the end.
-*/
+    console.clear();
 
-/*
+/*********************************************************************************/
+
 //3. VARIABLES AND DECLARATIONS
+console.log("VARIABLES AND DECLARATIONS");
+
     // var, let, const (introduced in ES 2015/ ES6)
 
     // 3.1) var
-    var username = 'Bharati Subramanain';
+    var username = 'Bharati Subramanian';
     console.log(username);
     username = 'Bharati' //can reassign
     console.log(username);
@@ -57,63 +61,75 @@
     var firstname = 'Bharati';
 
     // 3.2) let
-    let uname = 'Bharati Subramanain';
+    let uname = 'Bharati Subramanian';
     console.log(uname);
     uname = 'Bharati' //can reassign
     console.log(uname);
 
     // 3.3) const
-    const name = 'Bharati';
+    const reassign = 'Bharati';
     //cannot reassign const
-    //name = 'Sharada';
+    //reassign = 'Sharada';
     //const x; -> Not allowed; Have to assign values to const variables
-*/
+    console.clear();
 
-/*
+
 // 4. DATA TYPES IN JAVASCRIPT 
+console.log("DATA TYPES IN JS");
+
     // 4.1) Primitive Data Types 
         // 4.1.1) String
         const name = 'John Doe';
-        console.log(typeof name); //shows the data type of the variable
+        console.log("Datatype = ", typeof name); //shows the data type of the variable
+
         // 4.1.2) Number
         const age = 20;
-        console.log(typeof age); 
+        console.log("Datatype = ", typeof age); 
+
         // 4.1.3) Boolean
         const hasKids = false;
-        console.log(typeof hasKids);
+        console.log("Datatype = ", typeof hasKids);
+
         // 4.1.4) Null
         const car = null;
-        console.log(typeof car); //gives object
+        console.log("Datatype = ", typeof car); //gives object
+
         // 4.1.5) Undefined
         let test;
-        console.log(typeof test);
+        console.log("Datatype = ", typeof test);
+
         // 4.1.6) Symbol
         const sym = Symbol();
-        console.log(typeof sym);
+        console.log("Datatype = ", typeof sym);
+
     // 4.2) Reference Types
         // 4.2.1) Arrays
         const hobbies = ['books', 'movies', 'music'];
-        console.log(typeof hobbies);
+        console.log("Datatype = ", typeof hobbies);
+
         // 4.2.2) Object Literals
         const address = {
             city: 'Chennai',
             state: 'Tamil Nadu',
             country: 'India'
         };
-        console.log(typeof address);
+        console.log("Datatype = ", typeof address);
+
         // 4.2.3) Functions
         let func = function print() {
-            console.log("Inside print(): Hello world");
+            console.log("Datatype = ", "Inside print(): Hello world");
         };
-        console.log(typeof func);
+        console.log("Datatype = ", typeof func);
+
         // 4.2.4) Dates
         const date = new Date();
-        console.log(date);
-        console.log(typeof date);
-*/
+        console.log("Date = ", date);
+        console.log("Datatype = ", typeof date);
 
-/*
+/*********************************************************************************/
+
 // 5. TYPE CONVERSION 
+console.log("TYPE CONVERSION");
     //5.1) Type to string
     // Number to string
     val = String(5);
@@ -130,9 +146,9 @@
     val = (true).toString();
     // Date to string
     val = (new Date()).toString();
-    console.log(val);
-    console.log(typeof val);
-    console.log(val.length);
+    console.log("Value of variable = ", val);
+    console.log("Datatype of variable after conversion = ", typeof val);
+    console.log("Length of the value = ", val.length);
 
     //5.1) Type to Number
     // string to Number
@@ -145,26 +161,16 @@
     val = Number('hello'); //NaN -> Not a number
     val = parseInt('100'); //100
     val = parseFloat(100.30); // 100.3
-    console.log(val);
-    console.log(typeof val);
-    console.log(val.toFixed(2));
+    console.log("Value of variable = ", val);
+    console.log("Datatype of variable after conversion = ", typeof val);
+    console.log("Converting Number to a fixed decimal ", val.toFixed(2));
 
     //5.3) Type Coercion
     const val1 = String(5);
     const val2 = 6;
     const sum = val1 + val2;
-    console.log(sum);
-    console.log(typeof sum); //converts 6 to string
-*/
-
-
-
-
-
-
-
-
-    
+    console.log("Value of variable = ", sum);
+    console.log("Datatype of variable after conversion = ", typeof sum); //converts 6 to string   
 
 
 
