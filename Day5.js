5deb5345e2bd25f37895268183f79fa639574fbe
Bharati <bharatisharada@gmail.com>
2021-01-05 23:52:39 +0530

    Added If, conditional operators and switch to Day5.js

--- Day5.js
+++ Day5.js
@@ -82,3 +82,145 @@ console.log("My Birthday is on: ", d1);
     console.log("d.setMinutes(16) =",d1);
     d1.setSeconds(56);
     console.log("d.setSeconds(56) =",d1);
+
+console.clear();
+
+// 12. IF STATEMENTS AND CONDITIONAL OPERATORS
+
+/*
+    if(true) {
+        //do something
+    }
+    else {
+        // do something else
+    }
+*/
+
+const id = 100;
+
+    // 12.1) EQUAL TO (==)
+    if(id == 100) {
+        console.log("ID is equal to 100");
+    }
+    else {
+        console.log("ID is not equal to 100");
+    }
+
+    // 12.2) NOT EQUAL TO (!=)
+    if(id != 101) {
+        console.log("ID is not equal to 101");
+    }
+    else {
+        console.log("ID is equal to 101");
+    }
+
+    /*
+        == ignores type and returns true
+        e.g. '100' == 100 => returns true;
+
+        === compares the type and value
+        e.g. '100' === 100 => returns false;
+    */
+
+    /*
+        // To test if something is undefined:
+        if(typeof dummy !== 'undefined') {
+            console.log('The dummy =', dummy );
+        }
+        else {
+            console.log("Dummy variable is undefined");
+        }
+    */
+
+    // 12.3) GREATER THAN OR LESS THAN
+    if(id > 200) {
+        console.log("ID is greater than 200");
+    }
+    else {
+        console.log("ID is not greater than 200");
+    }
+
+    // 12.4) IF ELSE
+    const col = 'yellow';
+    if(col === 'red') {
+        console.log("Color is RED");
+    } else if(col === 'blue') {
+        console.log('Color is BLUE');
+    }
+    else {
+        console.log('Color is NOT RED OR BLUE. Color is', col)
+    }
+
+    // 12.5) LOGICAL OPERATORS
+        // 12.5.1) AND (&&)
+        const score = 95;
+        let grade = 'A';
+        if(score>90) {
+            console.log("OUTSTANDING SCORE! Score =", score, ", Grade = O");
+        }
+        else if(score>80 && score<=90) {
+            console.log("EXCELLENT SCORE! Score =", score, ", Grade = A");
+        }
+        else if(score>70 && score<=80) {
+            console.log("GOOD SCORE! Score =", score, ", Grade = B");
+        }
+        else if(score>60 && score<=70) {
+            console.log("AVERAGE SCORE! Score =", score, ", Grade = C");
+        }
+        else if(score>50 && score<=60) {
+            console.log("POOR SCORE! Score =", score, ", Grade = D");
+        }
+        else {
+            console.log("BETTER LUCK NEXT TIME! YOU FAILED THE TEST.");
+        }
+        let agge = 16;
+
+        // 12.5.2) OR (||)
+        if(agge<16 || agge>60) {
+            console.log("You cannot appear in the race");
+        }
+        else {
+            console.log("You have been registered to participate in the race! Age:", agge);
+        }
+    
+    // 12.6) TERNARY OPERATOR
+    console.log("id === 100 ? :", id === 100 ? "ID IS 100" : "ID IS NOT EQUAL TO 100");
+
+// 13. SWITCH
+switch(col) {
+    case 'red': 
+        console.log("The color is red");
+        break;
+    case 'blue':
+        console.log("The color is blue");
+        break;
+    case 'yellow':
+        console.log("The color is yellow");
+        break;
+    default:
+        console.log("The color is not red or blue or yellow. The color is", col);
+}
+
+switch(new Date().getDay()) {
+    case 0:
+        console.log("Today is Sunday");
+        break;
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    default:
+        console.log("Today is Saturday");
+        break;
+}
