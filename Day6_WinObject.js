7641c3de9eea2adc04c53d2a362ab1963b1eeffb
Bharati <bharatisharada@gmail.com>
2021-01-06 21:37:28 +0530

    Updated Day6_WinObject.js numbering

--- Day6_WinObject.js
+++ Day6_WinObject.js
@@ -1,19 +1,19 @@
-// 16. WINDOW METHOD, OBJECTS AND PROPERTIES
+// 17. WINDOW METHOD, OBJECTS AND PROPERTIES
 //window.console.log() <=> console.log()
 
     /*
-    16.1) alert()
+    17.1) alert()
     alert('This is an alert!');
     */
 
     /*
-    16.2) prompt()
+    17.2) prompt()
     const input = prompt();
     alert(input);
     */
     
     /*
-    16.3) confirm()
+    17.3) confirm()
     if(confirm('Are you sure?')) {
         console.log('Yes!');
     }
@@ -22,34 +22,34 @@
     }
     */
     
-    // 16.4) Properties
+    // 17.4) Properties
     let prop;
 
-        // 16.4.1) outerHeight
+        // 17.4.1) outerHeight
         prop = window.outerHeight;
         console.log('outerHeight',prop);
 
-        // 16.4.2) outerWidth
+        // 17.4.2) outerWidth
         prop = window.outerWidth;
         console.log('outerWidth',prop);
 
-        // 16.4.3) innerHeight
+        // 17.4.3) innerHeight
         prop = window.innerHeight;
         console.log('innerHeight',prop);
 
-        // 16.4.4) innerWidth
+        // 17.4.4) innerWidth
         prop = window.innerWidth;
         console.log('innerWidth',prop);
 
-        // 16.4.5) ScrollY
+        // 17.4.5) ScrollY
         prop = window.scrollY; //Gives position of vertical scroll bar
         console.log('scrollY',prop);
 
-        // 16.4.6) ScrollX
+        // 17.4.6) ScrollX
         prop = window.scrollX; //Gives position of vertical scroll bar
         console.log('scrollX',prop);
 
-        // 16.4.6) Location Object
+        // 17.4.6) Location Object
         prop = window.location; 
         console.log('location',prop);
         console.log('hostname', prop.hostname);
@@ -57,18 +57,18 @@
         console.log('href', prop.href);
         console.log('search', prop.search); //Query strings
         
-        // 16.4.7) Redirect
+        // 17.4.7) Redirect
         // window.location.href = 'https://www.google.com';
 
-        // 16.4.8) Reload
+        // 17.4.8) Reload
         //window.location.reload(); // constantly reloads the page
 
-        // 16.4.9) History object
+        // 17.4.9) History object
         //window.history.go(-1); //goes back one page in the history, -2: 2 pages, -3: 3 pages and so on
 
         console.log('Number of pages visited before the current page',window.history.length);
 
-        // 16.4.10) Navigator object 
+        // 17.4.10) Navigator object 
         // Has to do with the Browser and not the window
         prop = window.navigator;
         console.log('Navigator Object:', prop);
