4a472c5ea3bf0252c38472072de63d80756d89fc
Bharati <bharatisharada@gmail.com>
2021-01-15 22:48:46 +0530

    Replacing and Removing Elements

--- DOM/Day11.js
+++ DOM/Day11.js
@@ -25,3 +25,39 @@ const li = document.createElement('li');
     li.appendChild(link);
 
     console.log(li);
+
+// 8. REMOVING AND REPLACING ELEMENTS
+
+    // 8.1) Replacing elements
+    // Create a new element
+    const newHead = document.createElement('h2');
+    
+    // Create a net
+    newHead.id = 'task-title';
+
+    // New Text Node
+    newHead.appendChild(document.createTextNode('Task List'));
+
+    // Get the element to be replaced
+    const oldHead = document.getElementById('task-title');
+
+    // Get parent of the element to be replaced
+    const cardAction = document.querySelector('.card-action'); 
+
+    // Replace the old child with the new child
+    cardAction.replaceChild(newHead, oldHead);
+
+    console.log(newHead);
+
+
+    // 8.2) Removing elements
+    const lis = document.querySelectorAll('li');
+    const list = document.querySelector('ul');
+
+    // Remove list item
+    lis[lis.length-1].remove();
+
+    // Remove child element
+    list.removeChild(lis[3]);
+
+    
