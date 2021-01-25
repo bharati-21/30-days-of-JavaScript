447cb71b0748f13f260d94af1743f46ec6b10b4c
Bharati <bharatisharada@gmail.com>
2021-01-25 22:57:43 +0530

    Day 19: Local Persistent Storage functionality added to Project 1

--- DOM_Project_1/app.js
+++ DOM_Project_1/app.js
@@ -11,6 +11,10 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
+
+    // DOM load event
+    document.addEventListener('DOMContentLoaded', getTasksOnReload);
+
     // Add task event
     form.addEventListener('submit', addTask);
 
@@ -22,6 +26,40 @@ function loadEventListeners() {
 
     // Filtering tasks event
     filter.addEventListener('keyup', filterTasks);
+}
+
+// Get Tasks on reloading the page
+function getTasksOnReload (e) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(t) {
+        const li = document.createElement('li');
+
+        // Add class for list item
+        li.className = 'collection-item';
+
+        // Create TextNode and append it to list item with the user input
+        li.appendChild(document.createTextNode(t));
+
+        // Create a link for removing the list item
+        const link = document.createElement('a');
+
+        // Add link name
+        link.className = 'delete-item secondary-content';
+
+        // Add font awesome 'x' to the link
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+
+        // Append the child to list item
+        li.appendChild(link);
+        taskList.appendChild(li);
+    });
+
 }
 
 // Add task
@@ -45,25 +83,60 @@ function addTask(e) {
         // Append the child to list item
         li.appendChild(link);
 
-        console.log(li);
+        // console.log(li);
 
         // Append the list item to the list collection (ul)
         taskList.appendChild(li);
+
+        storeTaskInLocalStorage(taskInput.value);
 
         taskInput.value = "";
     }
 } 
 
+
+function storeTaskInLocalStorage(task) {
+    // Store task in local storage
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 // Delete item from task list
 function deleteItem(e) {
+    let task;
     if(e.target.parentElement.classList.contains('delete-item')) {
         console.log(e.target);
         if(confirm('Are you sure you want to remove this task?')) {
+            
             e.target.parentElement.parentElement.remove();
+
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement.textContent);
         }
         // On clicking gives <i></i>. Hence selecting parent element's parent element (li)
     }
 }
+
+
+function removeTaskFromLocalStorage(task) {
+    let tasksList = JSON.parse(localStorage.getItem('tasks'));
+
+    tasksList.forEach(function(t, index){
+        if(t === task) {
+            tasksList.splice(index, 1);
+        }
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasksList));
+}
+
 
 // Clear the task list
 function clearTasks(e) {
@@ -74,6 +147,7 @@ function clearTasks(e) {
     while(taskList.firstChild) {
         taskList.removeChild(taskList.firstChild);
     }
+    localStorage.removeItem('tasks');
 }
 
 // Filter task list items
@@ -90,8 +164,7 @@ function filterTasks(e) {
         }
     });
 
-
-
     // querySelector returns NodeList hence forEach() works
     // elementByClass returns HTML which needs to be converted into an Array before using forEach()
 }
+
