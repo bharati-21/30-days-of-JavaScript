7463e9fa93ca53faf637dc8a2937f46243332e06
Bharati <bharatisharada@gmail.com>
2021-02-16 23:10:26 +0530

    Day 27: Completed DOM Project 5

--- DOM_Project_5/app.js
+++ DOM_Project_5/app.js
@@ -1,6 +1,9 @@
 const convertBtn = document.getElementById("convert-btn");
 const inputTemp = document.getElementById("temp-cel");
 const outputTemp = document.getElementById("temp-fah");
+const headingDiv = document.getElementById("heading");
+const headerDiv = document.getElementById("header-div");
+
 
 convertBtn.addEventListener('click', convertToFahrenheit);
 
@@ -8,11 +11,30 @@ function convertToFahrenheit(e) {
     e.preventDefault();
     let tempInCelsius = parseFloat(inputTemp.value);
     if(isNaN(tempInCelsius)) {
-        alert("Please enter a valid temperature. Only Float or Integer values allowed");
+        raiseError();
         inputTemp.value="";
     }
     else {
         let tempInFahrenheit = (tempInCelsius * 9/5) + 32;
         outputTemp.value = tempInFahrenheit;
     }
+}
+
+function raiseError() {
+    /*
+    <div class="alert alert-danger" role="alert">
+                        A simple danger alert—check it out!
+                    </div>
+    */
+
+    const errorDiv = document.createElement("div");
+    errorDiv.className="alert alert-danger mt-5 text-center";
+    errorDiv.appendChild(document.createTextNode("Temperature input is invalid. Enter integer or float"));
+    headerDiv.insertBefore(errorDiv, headingDiv.nextSibling);
+
+    setTimeout(removerError, 3000);
+}
+
+function removerError() {
+    headingDiv.nextSibling.style.display = "none";
 }
